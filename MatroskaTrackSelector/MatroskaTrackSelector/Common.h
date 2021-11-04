4fac6517b8a0ddbcf36fe41a426a224d1e85d576
Kyoya2 <emenyrom@protonmail.com>
2021-11-04 20:33:57 +0200

    Init

---
+++ MatroskaTrackSelector/MatroskaTrackSelector/Common.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
---
+++ MatroskaTrackSelector/MatroskaTrackSelector/EbmlVint.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Common.h"
+
+class EbmlVint
+{
+public:
+    explicit EbmlVint(uint64_t value) : m_value
+
+private:
+    uint64_t m_value;
+};
+
