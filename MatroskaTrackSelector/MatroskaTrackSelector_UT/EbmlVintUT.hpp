a59ea53f03f9e023d8800c613b9f41abb20e9dd2
Kyoya2 <emenyrom@protonmail.com>
2021-11-06 00:34:59 +0200

    Add support for reading and writing to files

--- MatroskaTrackSelector/MatroskaTrackSelector_UT/EbmlVintUT.hpp
+++ MatroskaTrackSelector/MatroskaTrackSelector_UT/EbmlVintUT.hpp
@@ -1,26 +1,33 @@
 #include "UTCommon.hpp"
 
+#include <ios>
+#include <fstream>
+
 #define private public
 #include "EbmlVint.h"
 
 namespace EbmlVintUT
 {
-
     void run_tests()
     {
         WriteLine("EbmlVint tests");
 
-        // 20 test numbers of all possible encodable sizes (1-7 bytes) including highest and lowest
-        constexpr array<array<uint64_t, 20>, 7> test_numers
+        // 10 test numbers of all possible encodable sizes (1-7 bytes) including highest and lowest
+        constexpr array<array<uint64_t, 10>, 8> test_numers
         {{  // Lowest                Highest                       Random . . .
-            {(uint64_t)1 << (7 * 0), ((uint64_t)1 << (7 * 1)) - 1, 0, 6, 12, 38, 39, 63, 66, 75, 79, 85, 94, 106, 109, 110, 113, 118, 119, 126},
-            {(uint64_t)1 << (7 * 1), ((uint64_t)1 << (7 * 2)) - 1, 344, 1258, 1726, 1850, 2670, 3731, 4069, 4941, 8031, 8059, 8154, 9574, 11041, 11987, 12397, 13735, 14314, 14387},
-            {(uint64_t)1 << (7 * 2), ((uint64_t)1 << (7 * 3)) - 1, 282383, 369707, 527002, 544295, 697924, 766381, 829295, 904494, 1084195, 1102163, 1284567, 1412491, 1458529, 1540381, 1554385, 1619442, 1692230, 2094911},
-            {(uint64_t)1 << (7 * 3), ((uint64_t)1 << (7 * 4)) - 1, 30941096, 40242832, 53344747, 87263653, 103606745, 125765211, 127295766, 149243257, 159617931, 173255854, 190049517, 200751327, 202454090, 204947427, 244181730, 256084297, 258507919, 267626934},
-            {(uint64_t)1 << (7 * 4), ((uint64_t)1 << (7 * 5)) - 1, 4396930618, 4932096994, 5517407689, 5751543178, 6656613332, 7177549870, 7285656667, 8265809759, 8372419204, 10500762302, 10654861128, 13020989791, 18467861653, 20062413224, 20593554326, 23278062372, 27518318202, 32188437251},
-            {(uint64_t)1 << (7 * 5), ((uint64_t)1 << (7 * 6)) - 1, 36179364208, 547468931147, 832188127539, 1073607087742, 1153075541102, 1503870319197, 1515742034173, 2216009389489, 2250301607708, 2349425676110, 2528182596175, 2642358148140, 2999771855865, 3115379634907, 3368143188025, 3378068558223, 4136534312620, 4242814309487},
-            {(uint64_t)1 << (7 * 6), ((uint64_t)1 << (7 * 7)) - 1, 22901212075985, 48378207147389, 59891251192781, 71996117941362, 97536513290462, 118682338373531, 130700646706090, 134989253970622, 167914607363539, 187759699830931, 199758124950051, 200985149069771, 268014717834237, 296906899531551, 329422502702055, 481030464057176, 490839924807192, 550620692848821}
+            {(uint64_t)1 << (7 * 0), ((uint64_t)1 << (7 * 1)) - 1, 3, 21, 23, 33, 40, 48, 63, 104},
+{(uint64_t)1 << (7 * 1), ((uint64_t)1 << (7 * 2)) - 1, 2145, 2611, 5116, 5713, 6920, 7183, 8527, 13726},
+{(uint64_t)1 << (7 * 2), ((uint64_t)1 << (7 * 3)) - 1, 20192, 30931, 194895, 325646, 360101, 411640, 584949, 1020706},
+{(uint64_t)1 << (7 * 3), ((uint64_t)1 << (7 * 4)) - 1, 60760031, 74896690, 85532447, 162698622, 209156049, 230218164, 237236699, 247558183},
+{(uint64_t)1 << (7 * 4), ((uint64_t)1 << (7 * 5)) - 1, 7248751067, 7796102206, 16253194168, 16838031476, 19637100865, 21856134694, 31802121689, 32163113226},
+{(uint64_t)1 << (7 * 5), ((uint64_t)1 << (7 * 6)) - 1, 948972099695, 1147755987023, 1280105707974, 2971872828217, 3358800559283, 3781701395214, 4117206037367, 4339859932640},
+{(uint64_t)1 << (7 * 6), ((uint64_t)1 << (7 * 7)) - 1, 25096682569717, 65988423884461, 104265902063787, 312867211253539, 320614950036023, 349722492589874, 358762174973963, 368474031442049},
+{(uint64_t)1 << (7 * 7), ((uint64_t)1 << (7 * 8)) - 1, 12730655342155177, 13501103592526957, 16116897341298376, 24267091028906911, 29389454369744868, 44330399345006307, 55889445115127876, 59423846566273419}
         }};
+
+        // Init test file
+        std::fstream test_file;
+        test_file.open("test_file.txt", std::ios_base::binary | std::ios_base::out | std::ios_base::in | std::ios_base::trunc);
 
         for (uint64_t i = 0; i < test_numers.size(); ++i)
         {
@@ -41,11 +48,40 @@ namespace EbmlVintUT
                 // Remove marker
                 CHECK(with_marker._s_remove_vint_marker(with_marker.m_value) == test_number);
 
-                // Minimal size
+                // Check minimal size
+                CHECK(i + 1 == without_marker.m_minimal_encoded_size);
+                CHECK(i + 1 == with_marker.m_minimal_encoded_size);
+
+                // Write to file
+                size_t mid_pos = 0;
+                size_t end_pos = 0;
+                test_file.seekp(0);
+
+                without_marker.write(test_file);
+                mid_pos = test_file.tellp();
+                with_marker.write(test_file);
+                end_pos = test_file.tellp();
+
+                // Read from file in reversed order and verify stream positions
+                test_file.seekg(0);
+                with_marker = EbmlVint<true>(test_file);
+                CHECK(mid_pos == test_file.tellg());
+                without_marker = EbmlVint<false>(test_file);
+                CHECK(end_pos == test_file.tellg());
+
+                // Check values
+                CHECK(without_marker.m_value == test_number);
+                CHECK(with_marker.m_value == value_with_marker);
+
+                // Check minimal size
                 CHECK(i + 1 == without_marker.m_minimal_encoded_size);
                 CHECK(i + 1 == with_marker.m_minimal_encoded_size);
             }
         }
+
+        EbmlVint<false> qwe = 0;
+        cout << qwe;
+
     }
 
     void no_marker()
