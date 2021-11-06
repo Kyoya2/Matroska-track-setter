e60cf97a51bb3085c70c6877710ec7b0df0c2feb
Kyoya2 <emenyrom@protonmail.com>
2021-11-06 22:43:49 +0200

    start writing parser module

--- MatroskaTrackSelector/MatroskaTrackSelector/EbmlElement.h
+++ MatroskaTrackSelector/MatroskaTrackSelector/EbmlElement.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Common.h"
+#include "EbmlElementID.h"
+#include "EbmlElementLength.h"
+
+using EbmlElementSpecification = std::unordered_map<EbmlElementIDType, EbmlElementSpecification>;
+
+template <const EbmlElementSpecification& element_specificaion>
+class EbmlElement
+{
+
+private:
+    EbmlElementID m_id;
+    EbmlElementLength m_length;
+    EbmlElement* m_parent;
+    uint64_t m_position;
+};
