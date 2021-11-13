ef44755060f5cec3ef5976190cb3defcf761927e
Kyoya2 <emenyrom@protonmail.com>
2021-11-13 14:49:45 +0200

    Better tests for BasicSharedPtr

--- MatroskaTrackSelector/MatroskaTrackSelector_UT/BasicSharedPtrUT.h
+++ MatroskaTrackSelector/MatroskaTrackSelector_UT/BasicSharedPtrUT.h
@@ -7,57 +7,87 @@ namespace BasicSharedPtrUT
     uint32_t regular_ctor_calls;
     uint32_t child_ctor_calls;
     uint32_t dtor_calls;
+    uint32_t arg_ctor_calls;
+    uint32_t arg_copy_ctor_calls;
+    uint32_t arg_move_ctor_calls;
 
-    class Test
+    class TestArg
     {
     public:
-        Test() : m_parent(), m_self() { ++regular_ctor_calls; }
-        Test(BasicSharedPtr<Test> parent) : m_parent(parent), m_self() { ++child_ctor_calls; }
-        ~Test() { ++dtor_calls; }
+        TestArg() { ++arg_ctor_calls; };
+        TestArg(const TestArg& other) {  ++arg_copy_ctor_calls; }
+        TestArg& operator=(const TestArg& other) = delete;
+        TestArg(TestArg&& other) noexcept { ++arg_move_ctor_calls; }
+        TestArg& operator=(TestArg&& other) = delete;
+    };
 
-        void set_self(BasicSharedPtr<Test> self)
+    class TestPointee
+    {
+    public:
+        TestPointee(TestArg& ref) : m_parent(), m_self(), m_arg_ref(ref) { ++regular_ctor_calls; }
+        TestPointee(BasicSharedPtr<TestPointee> parent) : m_parent(parent), m_self(), m_arg_ref(parent->m_arg_ref) { ++child_ctor_calls; }
+        ~TestPointee() { ++dtor_calls; }
+
+        void set_self(BasicSharedPtr<TestPointee> self)
         {
             m_self = self;
             m_self.release_ownership_unsafe();
         }
-        BasicSharedPtr<Test> get_child()
+        BasicSharedPtr<TestPointee> get_child()
         {
-            BasicSharedPtr<Test> reult = BasicSharedPtr<Test>::make_shared(m_self);
+            BasicSharedPtr<TestPointee> reult = BasicSharedPtr<TestPointee>::make_shared(m_self);
             reult->set_self(reult);
             return reult;
         }
 
-        BasicSharedPtr<Test> m_parent;
-        BasicSharedPtr<Test> m_self;
+        BasicSharedPtr<TestPointee> m_parent;
+        BasicSharedPtr<TestPointee> m_self;
+        TestArg& m_arg_ref;
     };
 
     void helper_func()
     {
         regular_ctor_calls = 0;
+        child_ctor_calls = 0;
         dtor_calls = 0;
-        BasicSharedPtr<Test> a = BasicSharedPtr<Test>::make_shared();
+        arg_ctor_calls = 0;
+        arg_copy_ctor_calls = 0;
+        arg_move_ctor_calls = 0;
+
+        TestArg qwe;
+        CHECK(1 == arg_ctor_calls);
+
+        BasicSharedPtr<TestPointee> a = BasicSharedPtr<TestPointee>::make_shared(qwe);
         CHECK(1 == regular_ctor_calls);
         CHECK(0 == dtor_calls);
         CHECK(1 == a.get_refcount());
         CHECK(nullptr == a->m_self.m_internal_ptr);
+        CHECK(0 == child_ctor_calls);
+
+        CHECK(1 == arg_ctor_calls);
+        CHECK(0 == arg_copy_ctor_calls);
+        CHECK(0 == arg_move_ctor_calls);
 
         a->set_self(a);
         CHECK(1 == a.get_refcount());
 
-        BasicSharedPtr<Test> b_1 = a->get_child();
+        BasicSharedPtr<TestPointee> b_1 = a->get_child();
         CHECK(2 == a.get_refcount());
         CHECK(1 == b_1.get_refcount());
         CHECK(b_1.m_internal_ptr == b_1->m_self.m_internal_ptr);
+        CHECK(1 == child_ctor_calls);
 
-        BasicSharedPtr<Test> b_2 = a->get_child();
+        BasicSharedPtr<TestPointee> b_2 = a->get_child();
         CHECK(3 == a.get_refcount());
         CHECK(1 == b_2.get_refcount());
         CHECK(b_2.m_internal_ptr == b_2->m_self.m_internal_ptr);
+        CHECK(2 == child_ctor_calls);
 
-        BasicSharedPtr<Test> c_1 = b_1->get_child();
+        BasicSharedPtr<TestPointee> c_1 = b_1->get_child();
         CHECK(3 == a.get_refcount());
         CHECK(2 == b_1.get_refcount());
         CHECK(c_1.m_internal_ptr == c_1->m_self.m_internal_ptr);
+        CHECK(3 == child_ctor_calls);
     }
 
     void run_tests()
