d691e5e841edd6dbd89d67142beeae5d7a9bb809
Kyoya2 <emenyrom@protonmail.com>
2021-11-13 10:30:45 +0200

    Finish writing BasicSharedPtr and UT

--- MatroskaTrackSelector/MatroskaTrackSelector/BasicSharedPtr.h
+++ MatroskaTrackSelector/MatroskaTrackSelector/BasicSharedPtr.h
@@ -6,19 +6,28 @@
 template <typename T>
 class BasicSharedPtr
 {
-public:
     /******************************************************************************************************/
     /***************************************** (Con|De)structors ******************************************/
     /******************************************************************************************************/
-    BasicSharedPtr(T* ptr) :
+private:
+    BasicSharedPtr(T* ptr, size_t* refcount_ptr) :
         m_ptr(ptr),
-        m_refcount_ptr(new size_t(1))
+        m_refcount_ptr(refcount_ptr),
+        m_owned(true)
     {}
 
-    // Dopy
+public:
+    BasicSharedPtr() :
+        m_ptr(nullptr),
+        m_refcount_ptr(nullptr),
+        m_owned(false)
+    {}
+
+    // Copy
     BasicSharedPtr(const BasicSharedPtr& other) :
         m_ptr(other.m_ptr),
-        m_refcount_ptr(other.m_refcount_ptr)
+        m_refcount_ptr(other.m_refcount_ptr),
+        m_owned(true)
     {
         ++(*m_refcount_ptr);
     }
@@ -26,19 +35,47 @@ public:
     {
         m_ptr = other.m_ptr;
         m_refcount_ptr = other.m_refcount_ptr;
+        m_owned = true;
         ++(*m_refcount_ptr);
+
+        return *this;
     }
 
     // Move
-    BasicSharedPtr(BasicSharedPtr&& other) = delete;
-    BasicSharedPtr& operator=(BasicSharedPtr&& other) = delete;
+    BasicSharedPtr(BasicSharedPtr&& other) noexcept :
+        m_ptr(other.m_ptr),
+        m_refcount_ptr(other.m_refcount_ptr),
+        m_owned(other.m_owned) // Although there's no reason to move non-owned objects, it's still possible
+    {
+        other.m_owned = false;
+
+        // This is not really necessary but I'll leave it for a while to find potential bugs
+        other.m_ptr = nullptr;
+        other.m_refcount_ptr = nullptr;
+    }
+
+    BasicSharedPtr& operator=(BasicSharedPtr&& other) noexcept
+    {
+        m_ptr = other.m_ptr;
+        m_refcount_ptr = other.m_refcount_ptr;
+        m_owned = other.m_owned; // Although there's no reason to move non-owned objects, it's still possible
+
+        other.m_owned = false;
+        // This is not really necessary but I'll leave it for a while to find potential bugs
+        other.m_ptr = nullptr;
+        other.m_refcount_ptr = nullptr;
+
+        return *this;
+    }
  
     // Dtor
     ~BasicSharedPtr()
     {
-        if (0 == --(*m_refcount_ptr))
+        if (m_owned &&
+           (0 == --(*m_refcount_ptr)))
         {
-            delete m_ptr;
+            if (nullptr != m_ptr)
+                delete m_ptr;
             delete m_refcount_ptr;
         }
     }
@@ -46,8 +83,8 @@ public:
     /******************************************************************************************************/
     /********************************************* Operators **********************************************/
     /******************************************************************************************************/
-    T* operator->() { return m_ptr; }
-    T operator*() { return *m_ptr; }
+    inline T* operator->() { return m_ptr; }
+    inline T operator*() { return *m_ptr; }
 
     /******************************************************************************************************/
     /************************************************ Misc ************************************************/
@@ -55,21 +92,29 @@ public:
     template <class... Args>
     static BasicSharedPtr make_shared(Args&&... args)
     {
-        return BasicSharedPtr(new T(std::forward<Args>(args)...));
+        return BasicSharedPtr(new T(std::forward<Args>(args)...), new size_t(1));
     }
 
-    size_t get_refcount() { return *m_refcount_ptr; }
+    inline size_t get_refcount() { return *m_refcount_ptr; }
+
+    inline bool is_null() { return nullptr == m_ptr; }
 
     // Decreases refcount but doesn't invalidate object
     void release_ownership_unsafe()
     {
-        if (get_refcount() > 1)
+        if (get_refcount() > 1 && m_owned)
+        {
             --(*m_refcount_ptr);
+            m_owned = false;
+        }
         else
-            throw std::exception("Can't release ownership of single-owned object");
+        {
+            throw std::exception("Can't release ownership of single-owned object or can't release ownership twice");
+        }
     }
 
 private:
     T* m_ptr;
     size_t* m_refcount_ptr;
+    bool m_owned;
 };
--- MatroskaTrackSelector/MatroskaTrackSelector_UT/BasicSharedPtrUT.h
+++ MatroskaTrackSelector/MatroskaTrackSelector_UT/BasicSharedPtrUT.h
@@ -1,49 +1,69 @@
 #pragma once
 #include "UTCommon.h"
-
-#define private public
 #include "BasicSharedPtr.h"
 
 namespace BasicSharedPtrUT
 {
-    uint32_t ctor_calls;
+    uint32_t regular_ctor_calls;
+    uint32_t child_ctor_calls;
     uint32_t dtor_calls;
 
     class Test
     {
     public:
-        Test(int a, int b) : m_a(a), m_b(b) { ++ctor_calls; }
+        Test() : m_parent(), m_self() { ++regular_ctor_calls; }
+        Test(BasicSharedPtr<Test> parent) : m_parent(parent), m_self() { ++child_ctor_calls; }
         ~Test() { ++dtor_calls; }
 
-        int m_a;
-        int m_b;
+        void set_self(BasicSharedPtr<Test> self)
+        {
+            m_self = self;
+            m_self.release_ownership_unsafe();
+        }
+        BasicSharedPtr<Test> get_child()
+        {
+            BasicSharedPtr<Test> reult = BasicSharedPtr<Test>::make_shared(m_self);
+            reult->set_self(reult);
+            return reult;
+        }
+
+        BasicSharedPtr<Test> m_parent;
+        BasicSharedPtr<Test> m_self;
     };
 
     void helper_func()
     {
-        ctor_calls = 0;
+        regular_ctor_calls = 0;
         dtor_calls = 0;
-        BasicSharedPtr<Test> a = BasicSharedPtr<Test>::make_shared(1, 2);
-        CHECK(1 == a->m_a);
-        CHECK(2 == a->m_b);
-        CHECK(1 == ctor_calls);
+        BasicSharedPtr<Test> a = BasicSharedPtr<Test>::make_shared();
+        CHECK(1 == regular_ctor_calls);
         CHECK(0 == dtor_calls);
         CHECK(1 == a.get_refcount());
+        CHECK(nullptr == a->m_self.m_ptr);
 
-        BasicSharedPtr<Test> b = a;
-        CHECK(b.m_ptr == a.m_ptr);
-        CHECK(b.m_refcount_ptr == a.m_refcount_ptr);
-        CHECK(1 == b->m_a);
-        CHECK(2 == b->m_b);
-        CHECK(1 == ctor_calls);
-        CHECK(0 == dtor_calls);
-        CHECK(2 == b.get_refcount());
+        a->set_self(a);
+        CHECK(1 == a.get_refcount());
+
+        BasicSharedPtr<Test> b_1 = a->get_child();
+        CHECK(2 == a.get_refcount());
+        CHECK(1 == b_1.get_refcount());
+        CHECK(b_1.m_ptr == b_1->m_self.m_ptr);
+
+        BasicSharedPtr<Test> b_2 = a->get_child();
+        CHECK(3 == a.get_refcount());
+        CHECK(1 == b_2.get_refcount());
+        CHECK(b_2.m_ptr == b_2->m_self.m_ptr);
+
+        BasicSharedPtr<Test> c_1 = b_1->get_child();
+        CHECK(3 == a.get_refcount());
+        CHECK(2 == b_1.get_refcount());
+        CHECK(c_1.m_ptr == c_1->m_self.m_ptr);
     }
 
     void run_tests()
     {
         helper_func();
-        CHECK(1 == ctor_calls);
-        CHECK(1 == dtor_calls);
+        CHECK(1 == regular_ctor_calls);
+        CHECK(4 == dtor_calls);
     }
 }
