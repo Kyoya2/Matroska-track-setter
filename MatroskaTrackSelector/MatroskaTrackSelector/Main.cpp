f5df71140607c7edeb5b949c690c6e5b7df534c5
Kyoya2 <emenyrom@protonmail.com>
2021-11-18 20:04:10 +0200

    Change binary output directory and move tests to same project

--- MatroskaTrackSelector/MatroskaTrackSelector/Main.cpp
+++ MatroskaTrackSelector/MatroskaTrackSelector/Main.cpp
@@ -1,4 +1,13 @@
+#include "EbmlVintUT.h"
+#include "BasicSharedPtrUT.h"
+
 int wmain(int argc, wchar_t* argv[], wchar_t* [])
 {
-    int a;
+    EbmlVintUT::run_tests();
+    BasicSharedPtrUT::run_tests();
+
+    if (MatroskaTrackSelectorUT::tests_passed)
+        WriteLine(endl << "All test passed!");
+    else
+        WriteLine(endl << "Some tests FAILED :(");
 }
