f33b699c5719e2d3909acacc315153e6edfb28f5
Kyoya2 <emenyrom@protonmail.com>
2021-11-20 00:27:25 +0200

    Initial design of track parser and track element

--- MatroskaTrackSelector/MatroskaTrackSelector/TrackElement.h
+++ MatroskaTrackSelector/MatroskaTrackSelector/TrackElement.h
@@ -2,10 +2,31 @@
 #include "Common.h"
 #include "EbmlElement.h"
 
+enum class TrackLanguage
+{
+    Uninitialized = 0,
+    English,
+    Japanese,
+    Other
+};
+
 class TrackElement
 {
-    explicit TrackElement(std::iostream& stream);
+    explicit TrackElement(BasicSharedPtr<EbmlElement>& track_element);
 
 private:
+    // Elements
+    BasicSharedPtr<EbmlElement> m_name_element;
+    BasicSharedPtr<EbmlElement> m_language_element;
+    BasicSharedPtr<EbmlElement> m_language_ietf_element;
+    BasicSharedPtr<EbmlElement> m_flag_forced_element;
+    BasicSharedPtr<EbmlElement> m_flag_default_element;
 
+    // Element values
+    TrackType m_track_type;
+    string m_track_name;
+    uint64_t m_track_number;
+    TrackLanguage m_language;
+    bool m_forced;
+    bool m_default;
 };
--- MatroskaTrackSelector/MatroskaTrackSelector/TrackParser.h
+++ MatroskaTrackSelector/MatroskaTrackSelector/TrackParser.h
@@ -6,12 +6,13 @@
 class TrackParser
 {
 public:
-    TrackParser(std::iostream& stream);
+    explicit TrackParser(BasicSharedPtr<EbmlElement>& tracks_element);
 
 
 private:
     BasicSharedPtr<EbmlElement> m_tracks_seek_position;
     vector<BasicSharedPtr<EbmlElement>> m_top_level_void_elements;
-    vector<TrackElement> m_tracks;
+    vector<TrackElement> m_subtitle_tracks;
+    vector<TrackElement> m_audio_tracks;
 };
 
