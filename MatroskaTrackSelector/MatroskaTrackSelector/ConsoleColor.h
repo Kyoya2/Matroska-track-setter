259cebd8e1173dc2d095b862f35f7e4ee758b6ae
Kyoya2 <emenyrom@protonmail.com>
2021-12-09 23:02:42 +0200

    Added ConsoleColor module

---
+++ MatroskaTrackSelector/MatroskaTrackSelector/ConsoleColor.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Common.h"
+
+namespace ConsoleColor
+{
+    static const string_view BlackFG = "\x1b[30m";
+    static const string_view BlackBG = "\x1b[40m";
+    static const string_view RedFG = "\x1b[31m";
+    static const string_view RedBG = "\x1b[41m";
+    static const string_view GreenFG = "\x1b[32m";
+    static const string_view GreenBG = "\x1b[42m";
+    static const string_view YellowFG = "\x1b[33m";
+    static const string_view YellowBG = "\x1b[43m";
+    static const string_view BlueFG = "\x1b[34m";
+    static const string_view BlueBG = "\x1b[44m";
+    static const string_view MagentaFG = "\x1b[35m";
+    static const string_view MagentaBG = "\x1b[45m";
+    static const string_view CyanFG = "\x1b[36m";
+    static const string_view CyanBG = "\x1b[46m";
+    static const string_view LightGrayFG = "\x1b[37m";
+    static const string_view LightGrayBG = "\x1b[47m";
+    static const string_view DarkGrayFG = "\x1b[90m";
+    static const string_view DarkGrayBG = "\x1b[100m";
+    static const string_view LightRedFG = "\x1b[91m";
+    static const string_view LightRedBG = "\x1b[101m";
+    static const string_view LightGreenFG = "\x1b[92m";
+    static const string_view LightGreenBG = "\x1b[102m";
+    static const string_view LightYellowFG = "\x1b[93m";
+    static const string_view LightYellowBG = "\x1b[103m";
+    static const string_view LightBlueFG = "\x1b[94m";
+    static const string_view LightBlueBG = "\x1b[104m";
+    static const string_view LightMagentaFG = "\x1b[95m";
+    static const string_view LightMagentaBG = "\x1b[105m";
+    static const string_view LightCyanFG = "\x1b[96m";
+    static const string_view LightCyanBG = "\x1b[106m";
+    static const string_view WhiteFG = "\x1b[97m";
+    static const string_view WhiteBG = "\x1b[107m";
+}
