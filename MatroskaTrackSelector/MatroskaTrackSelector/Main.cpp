3aee1641dcec2eb9b2652a0a70548b70944246ff
Kyoya2 <emenyrom@protonmail.com>
2021-12-10 09:41:59 +0200

    Exclude tests from release build

--- MatroskaTrackSelector/MatroskaTrackSelector/Main.cpp
+++ MatroskaTrackSelector/MatroskaTrackSelector/Main.cpp
@@ -1,12 +1,12 @@
 ﻿#include <windows.h>
 
+#ifdef _DEBUG
 #include "EbmlVintUT.h"
 #include "BasicSharedPtrUT.h"
 #include "EbmlParserUT.h"
 #include "MatroskaLanguageTagsUT.h"
 #include "TrackParserUT.h"
 
-#ifdef _DEBUG
 namespace BasicSharedPtrStats
 {
     uint32_t total_creations = 0;
@@ -23,19 +23,20 @@ int wmain(int argc, wchar_t* argv[])
     // This is required for ConsoleUtils::print_table
     SetConsoleOutputCP(CP_UTF8);
 
-    //EbmlVintUT::run_tests();
-    //BasicSharedPtrUT::run_tests();
-    //EbmlParserUT::run_tests();
-    //MatroskaLanguageTagsUT::run_tests();
+#ifdef _DEBUG
+    EbmlVintUT::run_tests();
+    BasicSharedPtrUT::run_tests();
+    EbmlParserUT::run_tests();
+    MatroskaLanguageTagsUT::run_tests();
     TrackParserUT::run_tests();
 
-#ifdef _DEBUG
+
     WriteLine(endl << "Creations: " << BasicSharedPtrStats::total_creations);
     WriteLine("Deletions: " << BasicSharedPtrStats::total_deletions);
-#endif
 
     if (MatroskaTrackSelectorUT::tests_passed)
         WriteLine(endl << "All test passed!");
     else
         WriteLine(endl << "Some tests FAILED :(");
+#endif
 }
