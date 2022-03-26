cc65852e440a11e18322192b745d3fe2ac5cae2f
Kyoya2 <emenyrom@protonmail.com>
2022-03-27 00:28:49 +0300

    Start thinking of different colors for each priority

--- MatroskaTrackSelector/MatroskaTrackSelector/ConsoleUtils.h
+++ MatroskaTrackSelector/MatroskaTrackSelector/ConsoleUtils.h
@@ -28,40 +28,40 @@ DECL_EXCEPTION(InvalidTableContent);
 
 namespace ConsoleAttributes
 {
-    static const string_view BlackFG         = "\x1b[30m";
-    static const string_view BlackBG         = "\x1b[40m";
-    static const string_view RedFG           = "\x1b[31m";
-    static const string_view RedBG           = "\x1b[41m";
-    static const string_view GreenFG         = "\x1b[32m";
-    static const string_view GreenBG         = "\x1b[42m";
-    static const string_view YellowFG        = "\x1b[33m";
-    static const string_view YellowBG        = "\x1b[43m";
-    static const string_view BlueFG          = "\x1b[34m";
-    static const string_view BlueBG          = "\x1b[44m";
-    static const string_view MagentaFG       = "\x1b[35m";
-    static const string_view MagentaBG       = "\x1b[45m";
-    static const string_view CyanFG          = "\x1b[36m";
-    static const string_view CyanBG          = "\x1b[46m";
-    static const string_view LightGrayFG     = "\x1b[37m";
-    static const string_view LightGrayBG     = "\x1b[47m";
-    static const string_view DarkGrayFG      = "\x1b[90m";
-    static const string_view DarkGrayBG      = "\x1b[100m";
-    static const string_view LightRedFG      = "\x1b[91m";
-    static const string_view LightRedBG      = "\x1b[101m";
-    static const string_view LightGreenFG    = "\x1b[92m";
-    static const string_view LightGreenBG    = "\x1b[102m";
-    static const string_view LightYellowFG   = "\x1b[93m";
-    static const string_view LightYellowBG   = "\x1b[103m";
-    static const string_view LightBlueFG     = "\x1b[94m";
-    static const string_view LightBlueBG     = "\x1b[104m";
-    static const string_view LightMagentaFG  = "\x1b[95m";
-    static const string_view LightMagentaBG  = "\x1b[105m";
-    static const string_view LightCyanFG     = "\x1b[96m";
-    static const string_view LightCyanBG     = "\x1b[106m";
-    static const string_view WhiteFG         = "\x1b[97m";
-    static const string_view WhiteBG         = "\x1b[107m";
-    static const string_view Underline       = "\x1b[4m";
-    static const string_view NoUnderline     = "\x1b[24m";
+    static const string BlackFG         = "\x1b[30m";
+    static const string BlackBG         = "\x1b[40m";
+    static const string RedFG           = "\x1b[31m";
+    static const string RedBG           = "\x1b[41m";
+    static const string GreenFG         = "\x1b[32m";
+    static const string GreenBG         = "\x1b[42m";
+    static const string YellowFG        = "\x1b[33m";
+    static const string YellowBG        = "\x1b[43m";
+    static const string BlueFG          = "\x1b[34m";
+    static const string BlueBG          = "\x1b[44m";
+    static const string MagentaFG       = "\x1b[35m";
+    static const string MagentaBG       = "\x1b[45m";
+    static const string CyanFG          = "\x1b[36m";
+    static const string CyanBG          = "\x1b[46m";
+    static const string LightGrayFG     = "\x1b[37m";
+    static const string LightGrayBG     = "\x1b[47m";
+    static const string DarkGrayFG      = "\x1b[90m";
+    static const string DarkGrayBG      = "\x1b[100m";
+    static const string LightRedFG      = "\x1b[91m";
+    static const string LightRedBG      = "\x1b[101m";
+    static const string LightGreenFG    = "\x1b[92m";
+    static const string LightGreenBG    = "\x1b[102m";
+    static const string LightYellowFG   = "\x1b[93m";
+    static const string LightYellowBG   = "\x1b[103m";
+    static const string LightBlueFG     = "\x1b[94m";
+    static const string LightBlueBG     = "\x1b[104m";
+    static const string LightMagentaFG  = "\x1b[95m";
+    static const string LightMagentaBG  = "\x1b[105m";
+    static const string LightCyanFG     = "\x1b[96m";
+    static const string LightCyanBG     = "\x1b[106m";
+    static const string WhiteFG         = "\x1b[97m";
+    static const string WhiteBG         = "\x1b[107m";
+    static const string Underline       = "\x1b[4m";
+    static const string NoUnderline     = "\x1b[24m";
 }
 
 class ConsoleUtils
@@ -168,13 +168,15 @@ public:
 
 private:
     static size_t _s_get_console_text_size(const string& text)
-    {   //
-        //\x1b[100mBLA\x1b[4m
-        static const std::regex COLORED_TEXT("^\x1b\\[\\d{1,3}m.*\x1b\\[\\d{1,3}m$");
+    {
+        // A regex that matches text that is surrounded by one or more console colore escape codes
+        // and places the actual content (excluding the escape codes) in capturing group at index 1
+        static const std::regex COLORED_TEXT("^(?:\x1b\\[\\d{1,3}m)+(.*?)(?:\x1b\\[\\d{1,3}m)+$");
+        std::smatch match;
 
-        if (std::regex_search(text, COLORED_TEXT))
+        if (std::regex_search(text, match, COLORED_TEXT))
         {
-            return text.rfind('\x1b') - text.find('m') - 1;
+            return match[1].length();
         }
         return text.size();
     }
--- MatroskaTrackSelector/MatroskaTrackSelector/MinTrackEntry.cpp
+++ MatroskaTrackSelector/MatroskaTrackSelector/MinTrackEntry.cpp
@@ -3,33 +3,18 @@
 string MinTrackEntry::get_colored_name(const TrackPrioritizer& track_prioritizer) const
 {
     using namespace ConsoleAttributes;
-    static const string_view& EXPLICITLY_EXCLUDED_TRACKS_COLOR = LightRedFG;
-    static const string_view& NOT_INCLUDED_TRACKS_COLOR = LightYellowFG;
-    static const string_view& UNMATCHING_LUANGUAGE_TRACKS_COLOR = LightCyanFG;
-    static const string_view& TOP_PRIORITY_TRACKS_COLOR = LightGreenFG;
+    static const std::array<string, static_cast<size_t>(TrackPriorityDescriptor::NumberOfPriorities)> priority_colors = {
+        /* 0 */ WhiteFG,
+        /* 1 */ WhiteFG,
+        /* 2 */ WhiteFG,
+        /* 3 */ WhiteFG,
+        /* 4 */ WhiteFG,
+        /* 5 */ LightRedFG + RedBG,
+        /* 6 */ RedFG,
+    };
 
     string full_name = name.empty() ? ("Unnamed track " + std::to_string(unnamed_track_number)) : name;
-    const string_view* name_color = nullptr;
-    switch (track_prioritizer.get_track_priority(name, language))
-    {
-    case TrackPriorityDescriptor::ExplicitlyExcluded:
-        name_color = &EXPLICITLY_EXCLUDED_TRACKS_COLOR;
-        break;
-
-    case TrackPriorityDescriptor::NotIncluded:
-        name_color = &NOT_INCLUDED_TRACKS_COLOR;
-        break;
-
-    case TrackPriorityDescriptor::UnmatchingLanguage:
-        name_color = &UNMATCHING_LUANGUAGE_TRACKS_COLOR;
-        break;
-
-    case TrackPriorityDescriptor::TopPriority:
-        name_color = &TOP_PRIORITY_TRACKS_COLOR;
-        break;
-    }
-
-    return string(*name_color) + full_name + string(WhiteFG);
+    return priority_colors[static_cast<size_t>(track_prioritizer.get_track_priority(name, language))] + full_name + WhiteFG + BlackBG;
 }
 
 bool MinTrackEntry::operator<(const MinTrackEntry& other) const
