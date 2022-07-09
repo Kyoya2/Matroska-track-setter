36d4841e973669e48a215db50ca6998bdfebc2a3
Kyoya2 <emenyrom@protonmail.com>
2022-07-09 19:19:11 +0300

    Create SeekEntry class

---
+++ MatroskaTrackSelector/MatroskaTrackSelector/SeekEntry.cpp
@@ -0,0 +1,44 @@
+/*
+ *   Matroska track setter  Copyright (C) 2021  Kyoya2
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "SeekEntry.h"
+
+SeekEntry::SeekEntry(BasicSharedPtr<EbmlElement>& seek_element) :
+    seek_element(seek_element),
+    m_seek_position(0)
+{
+    unordered_map<EbmlElementIDType, BasicSharedPtr<EbmlElement>> children{
+        {SeekID_ID, nullptr},
+        {SeekPosition_ID, nullptr},
+    };
+
+    seek_element->get_unique_children(children);
+
+    // Save element pointers
+    seek_id_element = children[SeekID_ID];
+    seek_position_element = children[SeekPosition_ID];
+
+    // Load only the value of the seek ID
+    seek_id = static_cast<EbmlElementIDType>(children[SeekID_ID]->get_uint_value());
+}
+
+uint64_t SeekEntry::get_seek_position()
+{
+    if (0 == m_seek_position)
+        m_seek_position = seek_position_element->get_uint_value();
+
+    return m_seek_position;
+}
---
+++ MatroskaTrackSelector/MatroskaTrackSelector/SeekEntry.h
@@ -0,0 +1,45 @@
+/*
+ *   Matroska track setter  Copyright (C) 2021  Kyoya2
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "Common.h"
+#include "EbmlElement.h"
+
+class SeekEntry
+{
+public:
+    SeekEntry(BasicSharedPtr<EbmlElement>& seek_element);
+
+    SeekEntry(const SeekEntry&) = delete;
+    SeekEntry& operator=(const SeekEntry&) = delete;
+
+    SeekEntry(SeekEntry&& other) noexcept = default;
+    SeekEntry& operator=(SeekEntry&& other) noexcept = default;
+
+public:
+    uint64_t get_seek_position();
+
+public:
+    BasicSharedPtr<EbmlElement> seek_element;
+    BasicSharedPtr<EbmlElement> seek_id_element;
+    BasicSharedPtr<EbmlElement> seek_position_element;
+    EbmlElementIDType seek_id;
+
+PRIVATE:
+    uint64_t m_seek_position;
+};
+
