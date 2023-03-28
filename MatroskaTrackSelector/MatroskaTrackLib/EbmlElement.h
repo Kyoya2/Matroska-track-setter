c9d1292fdbb8fae465954c1c961b7ef5c08cbee1
Kyoya2 <emenyrom@protonmail.com>
2023-03-28 18:19:40 +0300

    Remove unnecesarry "_get_offset" and "_seek_to" function duplications

--- MatroskaTrackSelector/MatroskaTrackLib/EbmlElement.h
+++ MatroskaTrackSelector/MatroskaTrackLib/EbmlElement.h
@@ -4,15 +4,6 @@
 #include "Common.h"
 #include "EbmlElementBase.h"
 
-// Offsets relative to the current element
-enum class EbmlOffset
-{
-    Begin,                                 // Offset to the element ID
-    Length = (int)EbmlBasicOffset::Length, // Offset to the element's length
-    Data   = (int)EbmlBasicOffset::Data,   // Offset to the element's data
-    End    = (int)EbmlBasicOffset::End     // Offset to the end of the element
-};
-
 template <uint8_t id_size>
 class EbmlElement : public EbmlElementBase
 {
@@ -20,15 +11,6 @@ protected:
     using EbmlElementBase::EbmlElementBase;
 
 protected:
-    inline constexpr size_t _get_offset(EbmlOffset what) const noexcept;
-    inline void _seek_to(EbmlOffset where) const { _seek_stream(_get_offset(where)); }
+    inline constexpr size_t _get_element_start_offset() const noexcept { return _get_basic_offset(EbmlBasicOffset::Length) - id_size; }
+    inline void _seek_to_element_start() const { _seek_stream(_get_element_start_offset()); }
 };
-
-template <uint8_t id_size>
-constexpr size_t EbmlElement<id_size>::_get_offset(EbmlOffset what) const noexcept
-{
-    if (what == EbmlOffset::Begin)
-        return _get_basic_offset(EbmlBasicOffset::Length) - id_size;
-    else
-        return _get_basic_offset(static_cast<EbmlBasicOffset>(what));
-}
--- MatroskaTrackSelector/MatroskaTrackLib/EbmlPrimitiveElement.h
+++ MatroskaTrackSelector/MatroskaTrackLib/EbmlPrimitiveElement.h
@@ -61,7 +61,7 @@ const T& EbmlPrimitiveElement<id_size, T>::get_value() const
 {
     if (!m_is_initialized)
     {
-        EbmlElementBase::_basic_seek_to(EbmlOffset::Data);
+        EbmlElementBase::_basic_seek_to(EbmlBasicOffset::Data);
         _read_value(m_value);
         m_is_initialized = true;
     }
@@ -74,12 +74,12 @@ void EbmlPrimitiveElement<id_size, T>::set_value(const T& value, size_t encoded_
 {
     if (encoded_size != EbmlElementBase::m_data_length)
     {
-        EbmlElementBase::_basic_seek_to(EbmlOffset::Length);
+        EbmlElementBase::_basic_seek_to(EbmlBasicOffset::Length);
         EbmlElementBase::m_data_length = encoded_size;
         EbmlElementBase::_get_stream() << EbmlElement::m_data_length;
     }
     else
-        EbmlElementBase::_basic_seek_to(EbmlOffset::Data);
+        EbmlElementBase::_basic_seek_to(EbmlBasicOffset::Data);
 
     _write_value(value, encoded_size);
     m_value = value;
